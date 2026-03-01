162ea044c3b467cf1af5fd01bfd03bfe50298fcd
Ivaneudo <ivaneudo.dev@gmail.com>
2026-03-01 18:32:00 -0300

    feat[teste]: Configuração do primeiro teste, testeCreateTodoSuccess

--- src/test/java/com/testeTecnico/desafio_todolist/DesafioTodolistApplicationTests.java
+++ src/test/java/com/testeTecnico/desafio_todolist/DesafioTodolistApplicationTests.java
@@ -1,13 +1,52 @@
 package com.testeTecnico.desafio_todolist;
 
+import com.testeTecnico.desafio_todolist.entity.Todo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import java.awt.*;
+
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DesafioTodolistApplicationTests {
 
+	public DesafioTodolistApplicationTests() {}
+
+	@LocalServerPort
+	private int port;
+	private WebTestClient webTestClient;
+
+	@BeforeEach
+	void setUp() {
+		// Cria o WebTestClient manualmente - SEM @Autowired
+		this.webTestClient = WebTestClient.bindToServer()
+						.baseUrl("http://localhost:" + port)
+						.build();
+	}
+
 	@Test
-	void contextLoads() {
+	void testeCreateTodoSuccess() {
+		var todo = new Todo("Todo 1", "desc todo 1", false, 1);
+
+		webTestClient
+						.post()
+						.uri("/todo")
+						.bodyValue(todo)
+						.exchange()
+						.expectStatus().isOk()
+						.expectBody()
+						.jsonPath("$").isArray()
+						.jsonPath("$.length()").isEqualTo(1)
+						.jsonPath("$[0].name").isEqualTo(todo.getName())
+						.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+						.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+						.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+	}
+	@Test
+	void testeCreateTodoFailure() {
 	}
 
 }
