86600996210a045f3ed3b4e486b1e7122653df06
Ivaneudo <ivaneudo1984@gmai.com>
2026-03-01 13:51:16 -0300

    fiet[TodoService]: Criando o TodoService

---
+++ src/main/java/com/testeTecnico/desafio_todolist/service/TodoService.java
@@ -0,0 +1,39 @@
+package com.testeTecnico.desafio_todolist.service;
+
+import com.testeTecnico.desafio_todolist.entity.Todo;
+import com.testeTecnico.desafio_todolist.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+  private TodoRepository todoRepository;
+  public TodoService(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
+  public List<Todo> create(Todo todo) {
+    todoRepository.save(todo);
+    return list();
+  }
+  public List<Todo> list() {
+    // Filtra por prioridade e depois por nome.
+    Sort sort = Sort.by(Sort.Direction.ASC, "prioridade").descending().and(
+            Sort.by(Sort.Direction.ASC, "nome").ascending()
+    );
+    return todoRepository.findAll(sort);
+  }
+  public List<Todo> update(Todo todo) {
+    todoRepository.save(todo);
+
+    return list();
+  }
+  public List<Todo> delete(Long id) {
+    todoRepository.deleteById(id);
+    return list();
+  }
+
+}
