d2ded9cb930aa180571f451d8582294a8d4299a8
Ivaneudo <ivaneudo1984@gmai.com>
2026-03-01 13:28:45 -0300

    feat[entity] Criando o entity Todo

---
+++ src/main/java/com/testeTecnico/desafio_todolist/entity/Todo.java
@@ -0,0 +1,55 @@
+package com.testeTecnico.desafio_todolist.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String descricao;
+  private boolean realizado;
+  private int prioriadade;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public boolean isRealizado() {
+    return realizado;
+  }
+
+  public void setRealizado(boolean realizado) {
+    this.realizado = realizado;
+  }
+
+  public int getPrioriadade() {
+    return prioriadade;
+  }
+
+  public void setPrioriadade(int prioriadade) {
+    this.prioriadade = prioriadade;
+  }
+}
