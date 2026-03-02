e8a2ea194304f52a84dd4e122a8a4704341e8de6
Ivaneudo <ivaneudo.dev@gmail.com>
2026-03-01 21:39:21 -0300

    feat[pow]: Adicionando o swagger

--- src/main/java/com/testeTecnico/desafio_todolist/DesafioTodolistApplication.java
+++ src/main/java/com/testeTecnico/desafio_todolist/DesafioTodolistApplication.java
@@ -1,4 +1,4 @@
-package com.testeTecnico.desafio_todolist;
+package desafio_todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
